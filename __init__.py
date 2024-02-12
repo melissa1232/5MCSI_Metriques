6d243913d8f81912a443f4523608ea0766e448d8
melissa1232 <81170074+melissa1232@users.noreply.github.com>
2024-02-12 11:44:31 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,8 +12,8 @@ def hello_world():
     return render_template('hello.html') 
   
 @app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+def moncontact():
+    return render_template("contact.html")
 
 @app.route('/paris/')
 def meteo():
