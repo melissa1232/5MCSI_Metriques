8d0eb893c82231abd270e86df1ae2995433c1198
melissa1232 <81170074+melissa1232@users.noreply.github.com>
2024-02-12 10:18:10 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html') 
+  
   @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
