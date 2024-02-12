c86877c59a0441e7d1d0698a4f51399f687cd16a
melissa1232 <81170074+melissa1232@users.noreply.github.com>
2024-02-12 12:36:26 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -38,6 +38,12 @@ def monhistogramme():
 @app.route("/commits/")
 def moncommits():
     return render_template("commits.html")
+
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.minute
+        return jsonify({'minutes': minutes})
   
 if __name__ == "__main__":
   app.run(debug=True)
